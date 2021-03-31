eebc13886c5590bf76e8b45522dc71bed1f48220
Sergey Lobanov <lobanovjkee@gmail.com>
2021-03-31 15:49:54 +0300

    Lesson 2

---
+++ lesson_2/task_6.js
@@ -0,0 +1,17 @@
+// 6. Реализовать функцию с тремя параметрами: function mathOperation(arg1, arg2, operation), где arg1, arg2 – значения аргументов, operation – строка с названием операции. 
+// В зависимости от переданного значения операции выполнить одну из арифметических операций (использовать функции из пункта 5) и вернуть полученное значение (использовать switch).
+
+function mathOperation(arg1, arg2, operation) {
+    switch(operation) {
+        case '+':
+            return arg1 + arg2;
+        case '-':
+            return arg1 - arg2;
+        case '*':
+            return arg1 * arg2;
+        case '/':
+            return arg1 / arg2;
+        default:
+            return 'Введите знак математической операции (+, -, *, /)'
+    }
+}
