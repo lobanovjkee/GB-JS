e24bbc9deb47349a71b0a88e81c5dc1139354db7
Sergey Lobanov <lobanovjkee@gmail.com>
2021-04-07 04:12:17 +0300

    Поправил 2 задание
    Убрал функцию в объект и переписал функцию с использованием ключевого слова this.

--- lesson_4/task_2.js
+++ lesson_4/task_2.js
@@ -3,18 +3,18 @@
 // 2.2. Реализуйте такие объекты.
 // 2.3. Перенести функционал подсчета корзины на объектно-ориентированную базу.
 
-let basket = [
-	{ id: 1, name: "Яблоки", price: 40, quantity: 3 },
-	{ id: 2, name: "Малина", price: 32, quantity: 8 },
-	{ id: 3, name: "Молоко", price: 60, quantity: 2 },
-];
+let cart = {
+	items: [
+		{ id: 1, name: "Яблоки", price: 40, quantity: 3 },
+		{ id: 2, name: "Малина", price: 32, quantity: 8 },
+		{ id: 3, name: "Молоко", price: 60, quantity: 2 },
+	],
+	countCartPrice() {
+		return this.items.reduce(
+			(totalPrice, cartItem) => totalPrice + cartItem.price * cartItem.quantity,
+			0
+		);
+	},
+};
 
-function countBasketPrice(array) {
-	let summ = 0;
-	array.forEach((element) => {
-		summ += element.price * element.quantity;
-	});
-	return summ;
-}
-
-console.log(countBasketPrice(basket));
+console.log(basket.countBasketPrice(basket));
