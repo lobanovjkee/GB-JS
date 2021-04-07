533d6ce7cb533812610687e2e73dce474c1998ce
Sergey Lobanov <lobanovjkee@gmail.com>
2021-04-07 04:31:54 +0300

    Поправил 3 задание
    Добавил мысли по поводу методов объекта для различных ситуаций.

--- lesson_4/task_3.js
+++ lesson_4/task_3.js
@@ -6,20 +6,28 @@ let good = {
 	photo: String,
 	name: String,
 	additionalText: String, // бестселлер, хит и т.д.
+	price: Number,
 	info: {
 		type: String, // тип продукта (электроника, продовольствие, бытовая химия ...)
 		description: String,
 		characteristics: {
 			brand: String,
 			countryOfOrigin: String,
-            // и еще куча других характеристик, по типу  https://i.imgur.com/yLDfHjt.png или https://i.imgur.com/XUY7b3O.png
+			// и еще куча других характеристик, по типу  https://i.imgur.com/yLDfHjt.png или https://i.imgur.com/XUY7b3O.png
 		},
 	},
-	price: Number,
 	sale: {
 		isOnSale: Boolean,
 		saleMultiplier: Number, // число от 0 до 1
 	},
 	isAvailable: Boolean, // есть ли на  складе, либо в продаже
 	quantity: Number,
+	renderCatalog() {
+		// здесь отдает на отрисовку
+		this.photo, this.name, this.additionalText, this.price, this.sale;
+	},
+	renderGoodPage() {
+		// а здесь уже всю остальную информацию
+	},
+	// ну и так далее
 };
